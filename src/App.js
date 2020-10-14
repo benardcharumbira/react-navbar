4dcd0957771521c1cd63e9527edcc34a0e8e4962
Benard <benardcharumbira@gmail.com>
2020-10-14 16:35:14 +0200

    added responsive state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [clickStatus, setClickStatus] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar">
+      <div className="brand-title">Brand Name</div>
+      <span
+        onClick={() => setClickStatus(!clickStatus)}
+        className="toggle-button"
+      >
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </span>
+      <div
+        className={
+          !clickStatus ? "navbar-links" : "navbar-links navbar-linksActive"
+        }
+      >
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
